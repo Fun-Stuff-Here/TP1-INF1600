75d2241c8a66a9d083b2db07862c231a8d586239
Nicolas Depelteau <nicolas.depelteau@polymtl.ca>
2021-02-03 17:30:13 -0500

    ND: ripmeeeeee

--- exo1/exo1.c
+++ exo1/exo1.c
@@ -22,6 +22,58 @@ short hamming_encoding(const char c)
    short code = 0;
 
    // Completer la fonction ici
+   //map le char
+   for(unsigned i =0,j=2; i<8;i++,j++)
+   {
+      if(i==3 || i==4)
+         j++;
+      
+      code |= (c>>i)&(0x0001<<j);
+   }
+
+   //p0
+   short p0 =0;
+   for(unsigned i =2; i<=14; i+=2)
+   {
+      p0 |= (c>>i)& 0x0001;
+   }
+   code |= parity(p0);
+
+
+   //p1
+   short p1 = 0;
+      for(unsigned i =2; i<=14;)
+   {
+      if(i&0x0001)
+         i++;
+      else
+           i+=3;
+      
+      
+
+      p1 |= (c>>i)& 0x0001;
+   }
+   code |= (parity(p1)<<1);
+
+      //p2
+   short p2 = 0;
+      for(unsigned i =3; i<=14; i++)
+   {
+      if(i==7)
+         i+=4;
+
+      p2 |= (c>>i)& 0x0001;
+   }
+   code |= (parity(p2)<<3);
+
+   //p3
+   short p3 = 0;
+      for(unsigned i =7; i<=14; i++)
+   {
+
+      p3 |= (c>>i)& 0x0001;
+   }
+   code |= (parity(p3)<<7);
 
    return code;
 }
