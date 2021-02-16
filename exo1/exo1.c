8507875092f82e3c83205d785444207eaf7780ce
Nicolas Depelteau <nicolas.depelteau@polymtl.ca>
2021-02-16 13:02:06 -0500

    ND:exo1

--- exo1/exo1.c
+++ exo1/exo1.c
@@ -6,12 +6,11 @@
 short parity(const short c)
 {
    short p = 0;
-   
 
    // Completer la fonction ici
-   for (unsigned i = 0; i < sizeof(c)*8; i++)
+   for (unsigned i = 0; i < sizeof(c) * 8; i++)
    {
-      p ^= (c>>i) & 1;
+      p ^= (c >> i) & 1;
    }
 
    return p;
@@ -21,126 +20,57 @@ short hamming_encoding(const char c)
 {
    short code = 0;
 
-   // Completer la fonction ici
-    for (unsigned i = 0,j=2; i <8; i++,j++)
-    {
-        if (j == 3 || j == 7)
-            j++;
-        if((c >> i) & 1)
-            code |= 1<<j;
-    }
+   code |= (c & 0x01) << 2;
+   code |= (c & 0x0E) << 3;
+   code |= (c & 0xF0) << 4;
 
-    //p0
-    code |=
-        parity((code & (1<<2)) |
-            (code & (1<<4)) |
-            (code & (1<<6)) |
-            (code & (1<<8)) |
-            (code & (1<<10)) |
-            (code & (1<<12)) |
-            (code & (1<<14)));
+   //p0
+   code |= parity(code & 0x1554) << 0;
 
-    //p1
-    code |=
-        parity((code & (1<<2)) |
-            (code & (1<<5)) |
-            (code & (1<<6)) |
-            (code & (1<<9)) |
-            (code & (1<<10)) |
-            (code & (1<<13)) |
-            (code & (1<<14)))
-        << 1;
-    //p2
-     code |=
-        parity((code & (1<<4)) |
-            (code & (1<<5)) |
-            (code & (1<<6)) |
-            (code & (1<<11)) |
-            (code & (1<<12)) |
-            (code & (1<<13)) |
-            (code & (1<<14)))
-        << 3;
-    //p3
-     code |=
-        parity((code & (1<<8)) |
-            (code & (1<<9)) |
-            (code & (1<<10)) |
-            (code & (1<<11)) |
-            (code & (1<<12)) |
-            (code & (1<<13)) |
-            (code & (1<<14)))
-        << 7;
+   //p1
+   code |= parity(code & 0x6664) << 1;
+
+   //p2
+   code |= parity(code & 0xf070) << 3;
+
+   //p3
+   code |= parity(code & 0xff00) << 7;
 
    return code;
 }
 
-char hamming_decoding(const short ccc)
+char hamming_decoding(const short c)
 {
    // Completer la fonction ici
 
-   short c = ccc;
+   short ccc = c;
    short cc = 0;
 
    //c0
-   cc |=
-        parity((c & (1)) |
-            (c & (1<<2)) |
-            (c & (1<<4)) |
-            (c & (1<<6)) |
-            (c & (1<<8)) |
-            (c & (1<<10)) |
-            (c & (1<<12))|
-            (c & (1<<14)) 
-            );
+   cc |= parity(ccc & 0x5555);
 
    //c1
-   cc |=
-        parity((c & (1<<1)) |
-            (c & (1<<2)) |
-            (c & (1<<5)) |
-            (c & (1<<6)) |
-            (c & (1<<9)) |
-            (c & (1<<10)) |
-            (c & (1<<13)) |
-            (c & (1<<14)))
-            <<1;
-   
+   cc |= parity(ccc & 0x6666) << 1;
+
    //c2
-   cc |=
-        parity((c & (1<<3)) |
-            (c & (1<<4)) |
-            (c & (1<<5)) |
-            (c & (1<<6)) |
-            (c & (1<<11)) |
-            (c & (1<<12)) |
-            (c & (1<<13)) |
-            (c & (1<<14)))
-            <<2;
+   cc |= parity(ccc & 0x7878) << 2;
 
    //c3
-   cc |=
-        parity((c & (1<<7)) |
-            (c & (1<<8)) |
-            (c & (1<<9)) |
-            (c & (1<<10)) |
-            (c & (1<<11)) |
-            (c & (1<<12)) |
-            (c & (1<<13)) |
-            (c & (1<<14)))
-            <<3;
+   cc |= parity(ccc & 0x7f80) << 3;
 
-   if(cc)
+   if (cc) //une erreur
    {
-      if(((cc-1)>>c)&1)
-         c |= 1<<(cc-1);
-      else
-         c ^= 1<<(cc-1);
+      //position cc-1
+      if (((cc - 1) >> ccc) & 1) //push un 1
+         ccc |= 1 << (cc - 1);
+      else                       //push un 0
+         ccc ^= 1 << (cc - 1);
    }
 
    char code = 0;
-   code |= (c & 0x0004) >> 2;
-   code |= (c & 0x0070) >> 3;
-   code |= (c & 0x0F00) >> 4;
+   code |= (ccc & 0x0004) >> 2;
+   code |= (ccc & 0x0070) >> 3;
+   code |= (ccc & 0x0F00) >> 4;
 
    return code;
 }
